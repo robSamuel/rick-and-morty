6e597d194da957d949055a11020f674240a8eaba
Robert Samuel <robert_samuel97@hotmail.com>
2021-04-06 23:00:55 -0600

    Styles - Create the scss files

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
+import "../assets/scss/main.scss";
 import "./layout.css";
 
 const Layout = ({ children }) => {
@@ -29,7 +30,6 @@ const Layout = ({ children }) => {
       <div
           style={{
               margin: `0 auto`,
-              maxWidth: 960,
               padding: `0 1.0875rem 1.45rem`,
           }}
       >
