8e7bc3e921c71ebc205842c629358df08b42ff81
Robert Samuel <robert_samuel97@hotmail.com>
2021-04-10 08:37:14 -0600

    Layout - Move the Layout component to its own folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
@@ -19,4 +19,4 @@ const IndexPage = () => (
   </Layout>
 );
 
-export default IndexPage
+export default IndexPage;
