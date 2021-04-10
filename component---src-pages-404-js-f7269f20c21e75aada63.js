26f3a7c7f7f58e06c564b5005784d410a0ae26ee
Robert Samuel <robert_samuel97@hotmail.com>
2021-04-10 09:06:05 -0600

    Updates

---
+++ component---src-pages-404-js-f7269f20c21e75aada63.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),l=n("ntAx"),o=n("vrFN");e.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f7269f20c21e75aada63.js.map
