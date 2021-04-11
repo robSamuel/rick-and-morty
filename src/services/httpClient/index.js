8cd6647eaa39b61cd7213850d55ec019068c7b9d
Robert Samuel <robert_samuel97@hotmail.com>
2021-04-10 19:54:22 -0600

    Http Client - Create the httpClient function

---
+++ src/services/httpClient/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const httpClient = ({ method, url, params }) => {
+    return axios({
+        method: method || 'GET',
+        baseURL: `${process.env.GATSBY_API_URL}`,
+        url: url || '',
+        params: { ...params }
+    });
+};
