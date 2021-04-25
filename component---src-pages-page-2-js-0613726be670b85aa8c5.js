15d17dd1688947a424b4b05d84900f8d49c3fe78
Robert Samuel <robert_samuel97@hotmail.com>
2021-04-25 03:18:41 -0600

    Updates

---
+++ component---src-pages-page-2-js-0613726be670b85aa8c5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{p5nM:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),l=n("Wbzz"),c=n("ntAx"),r=n("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-0613726be670b85aa8c5.js.map
