9476dafcd473aa9b77ddafdac7ebab93489e65dd
Robert Samuel <robert_samuel97@hotmail.com>
2021-05-22 21:53:51 -0600

    Add the routing configuration to the gatsby-ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+exports.createPages = function({actions}) {
+    actions.createPage({
+        path: 'character',
+        matchPath: '/character/:id',
+        component: require.resolve('./src/pages/character.jsx')
+    });
+};
