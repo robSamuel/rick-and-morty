21c144f6284eeed501b5b8021e2e411fded5caf5
Robert Samuel <robert_samuel97@hotmail.com>
2021-05-22 20:12:04 -0600

    List - Add the Titled List and allow the component to receive a list from the props

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState , useEffect, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import { isNotEmptyArray } from '../../utils';
 import Card from '../Card';
@@ -13,14 +13,24 @@ const List = props => {
     const [hasMore, setHasMore] = useState(true);
     const [list, setList] = useState([]);
     const [pagination, setPagination] = useState(initPagination);
-    const { link, retrieveData } = props;
-    const containerId = `ScrollContainer-${link}`;
+    const {
+        inheritedList,
+        link,
+        listTitle,
+        retrieveData,
+        useList
+    } = props;
 
     useEffect(() => {
-        fetchData();
+        if(!useList){
+            fetchData();
+        } else {
+            setList(inheritedList);
+            setHasMore(false);
+        }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [inheritedList]);
 
     const fetchData = async () => {
         const newPage = pagination.pages + 1;
@@ -50,6 +60,17 @@ const List = props => {
         }
     };
 
+    const renderTitle = () => {
+        if(!listTitle)
+            return <Fragment />;
+
+        return (
+            <label className="List-title">
+                {`${listTitle}:`}
+            </label>
+        )
+    };
+
     const renderList = () => {
         if(isNotEmptyArray(list)) {
             return list.map(item => {
@@ -74,8 +95,10 @@ const List = props => {
         );
     };
 
-    return (
-        <div className="List">
+    const renderInfiniteScroll = () => {
+        const containerId = `ScrollContainer-${link}`;
+
+        return (
             <div
                 className="List-container"
                 id={containerId}
@@ -91,17 +114,48 @@ const List = props => {
                     {renderList()}
                 </InfiniteScroll>
             </div>
-        </div>
-    );
+        );
+    };
+
+    const renderGeneralList = () => {
+        return (
+            <div className="List">
+                {renderInfiniteScroll()}
+            </div>
+        );
+    };
+
+    const renderTitledList = () => {
+        return (
+            <div className="List List-title-container">
+                {renderTitle()}
+                {renderInfiniteScroll()}
+            </div>
+        );
+    };
+
+    return !listTitle
+        ? renderGeneralList()
+        : renderTitledList();
+};
+
+List.defaultProps = {
+    inheritedList: [],
+    listTitle: '',
+    retrieveData: () => {},
+    useList: false
 };
 
 List.propTypes = {
+    inheritedList: PropTypes.array,
     link: PropTypes.oneOf([
         'character',
         'episode',
         'location']
     ).isRequired,
-    retrieveData: PropTypes.func.isRequired
+    listTitle: PropTypes.string,
+    retrieveData: PropTypes.func,
+    useList: PropTypes.bool
 };
 
 export default List;
